2486e210ddc9453d5003d1f7f598d449e9073ca1
Jean Diaz <jeanpaul1304@gmail.com>
2018-11-15 04:13:40 -0500

    first commit

---
+++ dist/precache-manifest.c7ed0eab7bf11a597c6e1c7b2b4f57a2.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "257809946b3dd8ef3b1991cad31d2aa7",
+    "url": "/dist/img/logotipo.25780994.jpg"
+  },
+  {
+    "revision": "fb50b0105565781eb32327babe9b4a38",
+    "url": "/dist/img/order-status.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/dist/robots.txt"
+  },
+  {
+    "revision": "42cf8e136406fde9f116",
+    "url": "/dist/js/chunk-vendors.f2a09254.js"
+  },
+  {
+    "revision": "c733e26c165753372204ca9b6419f3a3",
+    "url": "/dist/index.html"
+  },
+  {
+    "revision": "b8926a24373f5e50bc14791867e1a7a1",
+    "url": "/dist/img/perfil.b8926a24.png"
+  },
+  {
+    "revision": "ebd412d2c49348bab5c9",
+    "url": "/dist/js/app.e00081fb.js"
+  },
+  {
+    "revision": "8e35506a50bbc4c6762d3ddc890e9733",
+    "url": "/dist/img/menu.8e35506a.png"
+  },
+  {
+    "revision": "e21c83ec8a3f195c1b8ff58e783db0a2",
+    "url": "/dist/img/historial.e21c83ec.png"
+  },
+  {
+    "revision": "1644f3c23fc0e66715b9e0019f218509",
+    "url": "/dist/img/curso.1644f3c2.png"
+  },
+  {
+    "revision": "6dfcb9bfe99df4f4f5cfe02482cc1706",
+    "url": "/dist/img/contacto.6dfcb9bf.png"
+  },
+  {
+    "revision": "bd4939d3456ede3e6ab4fbb1e87405bd",
+    "url": "/dist/img/chef.png"
+  },
+  {
+    "revision": "dfd72c6dd3eb7cc86fd0455f8f891f17",
+    "url": "/dist/img/chef.dfd72c6d.png"
+  },
+  {
+    "revision": "2641d1bf535ea0c6e472f263c1a42e00",
+    "url": "/dist/img/chef-hat.2641d1bf.png"
+  },
+  {
+    "revision": "96be93b1113c8aa52abf60bb890d898a",
+    "url": "/dist/img/bg.96be93b1.jpg"
+  },
+  {
+    "revision": "42cf8e136406fde9f116",
+    "url": "/dist/css/chunk-vendors.51a27a13.css"
+  },
+  {
+    "revision": "ebd412d2c49348bab5c9",
+    "url": "/dist/css/app.c463c486.css"
+  }
+];
